ca4b5b05ba9727b3e3547af251073a8e368ad3b1
alexandremulina <49921028+alexandremulina@users.noreply.github.com>
2021-02-14 16:45:53 -0300

    List of Banks

--- Code/app.py
+++ Code/app.py
@@ -6,12 +6,30 @@ app = Flask(__name__)
 @app.route("/")
 
 def home():
-    r = requests.get("https://api.itau/open-banking/channels/v1/branches")
+    for bank in response_json:
+        bank_dict = dict()
+        bank_dict["name"] = bank['LegalEntityName']
+        bank_dict["status"] = bank['Status']
+        print(bank['LegalEntityName'])
+        auth_servers = bank['AuthorisationServers']
+        for server in auth_servers:
+            api_resources = server['ApiResources']
+            if api_resources is not None:
+                for api_resource in api_resources:
+                    api_family_type = api_resource['ApiFamilyType']
+                    try:
+                        api = api_resource['ApiDiscoveryEndpoints'][0]['ApiEndpoint']
+                    except IndexError:
+                        api = 'Null'
+                    print(api_family_type)
+                    print(api)
+                    bank_dict[f"api-{api_family_type}"] = api
+                    banks.append(bank_dict)
+    r = requests.get("https://api.itau/open-banking/channels/v1/branches") #loop  through ApiEndpoint
     r2 = requests.get("https://api.bradesco.com/bradesco/open-banking/channels/v1/branche")
     codes = [r.status_code, r2.status_code]
     results = []
     for code in codes:
-
         if code == 200:
             results.append('EM FUNCIONAMENTO')
         else:
