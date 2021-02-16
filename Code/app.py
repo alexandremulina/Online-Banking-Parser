5f7b8df129d2a76deaacc23de42224fbb238d0eb
Alan Mareines <alanmareines@gmail.com>
2021-02-16 16:02:28 -0300

    front-end init + venv for mac/linux

--- Code/app.py
+++ Code/app.py
@@ -1,48 +1,20 @@
 from flask import Flask, url_for, render_template
 import requests
+import test
 
 app = Flask(__name__)
 
 @app.route("/")
 
 def home():
-    for bank in response_json:
-        bank_dict = dict()
-        bank_dict["name"] = bank['LegalEntityName']
-        bank_dict["status"] = bank['Status']
-        auth_servers = bank['AuthorisationServers']
-        for server in auth_servers:
-            api_resources = server['ApiResources']
-            if api_resources is not None:
-                for api_resource in api_resources:
-                    api_family_type = api_resource['ApiFamilyType']
-                    try:
-                        api = api_resource['ApiDiscoveryEndpoints'][0]['ApiEndpoint']
-                    except IndexError:
-                        api = 'Null'
-                    bank_dict[f"api-{api_family_type}"] = api
-                banks.append(bank_dict)
-    for index in range(len(banks)):
-        # print(banks[index]['name'])
-        # print(banks[index]['status'])
-        try:
-            url = banks[index]['api-discovery']
-            url2 = banks[index]['api-discovery']
-            try:
-                # print('status code :', requests.get(url))
-            except requests.exceptions.ConnectionError:
-                # print("Connection Refused")
-        except KeyError:
-            url = 'Vazio'
-        # print(url)
-    codes = [r.status_code, r2.status_code]
-    results = []
-    for code in codes:
-        if code == 200:
-            results.append('EM FUNCIONAMENTO')
-        else:
-            results.append('SERVIÇO INSDISPONIVEL')
-    return render_template("index.html", content = results)
+    url = 'https://data.directory.openbankingbrasil.org.br/participants'
+    response_json = test.directory_api_call(url)
+    banks = test.parse_directory_response(response_json)
+
+    for bank in banks:
+        print(bank)
+        
+    return render_template("index.html", content = banks)
 
 
 
