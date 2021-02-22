bf8b094b3f20078eed30f5e0d3033849273c8741
Alan Mareines <alanmareines@gmail.com>
2021-02-22 20:06:57 -0300

    added coments for home()

--- Code/app.py
+++ Code/app.py
@@ -11,10 +11,14 @@ app = Flask(__name__)
 
 def home():
     url = 'https://data.directory.openbankingbrasil.org.br/participants'
+    # if db data > 2 hours | db is empty:
+    # make API calls
+    # save on db
+    # else:
+    # fetch from db (get)
     response_json = test.directory_api_call(url)
     banks = test.parse_directory_response(response_json)
     today = datetime.date.today()
-    print(banks)
     return render_template("index.html", content = banks, date_time = today)
 
 
