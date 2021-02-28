4a4639ab020c78dc4258deeae65f55b7c9c6f717
alexandremulina <49921028+alexandremulina@users.noreply.github.com>
2021-02-28 18:25:59 -0300

    add flask caching

--- Code/app.py
+++ Code/app.py
@@ -1,14 +1,22 @@
 from flask import Flask, url_for, render_template
-import requests
+from flask_caching import Cache
 import test
 import datetime
 
+config = {
+    "DEBUG": True,          # some Flask specific configs
+    "CACHE_TYPE": "simple", # Flask-Caching related configs
+    "CACHE_DEFAULT_TIMEOUT": 300
+}
 app = Flask(__name__)
+# tell Flask to use the above defined config
+app.config.from_mapping(config)
+cache = Cache(app)
+
+
 
 @app.route("/")
-
-
-
+@cache.cached(timeout=3600) # 1 hour
 def home():
     url = 'https://data.directory.openbankingbrasil.org.br/participants'
     # TODO if db data > 2 hours | db is empty:
