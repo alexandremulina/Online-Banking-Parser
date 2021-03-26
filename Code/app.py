77bf4925535b2dcedae6097cb3c55718d3d7f2b7
alexandremulina <49921028+alexandremulina@users.noreply.github.com>
2021-03-26 01:49:44 -0300

    add flask caching

--- Code/app.py
+++ Code/app.py
@@ -1,35 +1,46 @@
-from flask import Flask, url_for, render_template
-from flask_caching import Cache
+from flask import Flask, render_template
+from celery import group, shared_task
 import test
 import datetime
+import mongodb
+from celery import Celery
+from celery import task
+from flask_caching import Cache
+import schedule
+import time
 
-config = {
-    "DEBUG": True,          # some Flask specific configs
-    "CACHE_TYPE": "simple", # Flask-Caching related configs
-    "CACHE_DEFAULT_TIMEOUT": 300
-}
+
+
 app = Flask(__name__)
-# tell Flask to use the above defined config
-app.config.from_mapping(config)
-cache = Cache(app)
+app.config.from_object("config")
+app.secret_key = app.config['SECRET_KEY']
+
+
+client = Celery(app.name, broker=app.config['CELERY_BROKER_URL'])
+# client.conf.update(app.config)
+
+@client.task
+def update():
+    schedule.every(30).minutes.do(mongodb.insert_list())
+
+
 
 
 
 @app.route("/")
-@cache.cached(timeout=3600) # 1 hour
+# @cache.cached(timeout=3600) # 1 hour
 def home():
-    url = 'https://data.directory.openbankingbrasil.org.br/participants'
-    response_json = test.directory_api_call(url)
-    banks = test.parse_directory_response(response_json)
+    banks = mongodb.export_list()
     today = datetime.date.today()
-    return render_template("index.html", content = banks, date_time = today)
+    # update.apply_async()
+    return render_template("index.html", content=banks, date_time=today)
 
 
 
 
-if __name__=='__main__':
-    app.run()
 
+if __name__ == '__main__':
+    app.run(debug=True)
 
 
 
