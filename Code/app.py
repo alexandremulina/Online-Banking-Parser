05ec6dbac1db1036bb21b6c4ea319110aa70cb78
alexandremulina <49921028+alexandremulina@users.noreply.github.com>
2021-03-26 14:14:50 -0300

    add flask caching

--- Code/app.py
+++ Code/app.py
@@ -1,27 +1,25 @@
 from flask import Flask, render_template
-from celery import group, shared_task
 import test
 import datetime
 import mongodb
-from celery import Celery
-from celery import task
-from flask_caching import Cache
-import schedule
 import time
+from flask_apscheduler import APScheduler
+from apscheduler.schedulers.background import BackgroundScheduler
+app = Flask(__name__)
 
+
+
+
+def update():
+    mongodb.insert_list()
+
+sched = BackgroundScheduler(daemon=True)
+sched.add_job(update,'interval', minutes=5)
+sched.start()
 
 
 app = Flask(__name__)
-app.config.from_object("config")
-app.secret_key = app.config['SECRET_KEY']
 
-
-client = Celery(app.name, broker=app.config['CELERY_BROKER_URL'])
-# client.conf.update(app.config)
-
-@client.task
-def update():
-    schedule.every(30).minutes.do(mongodb.insert_list())
 
 
 
@@ -32,7 +30,6 @@ def update():
 def home():
     banks = mongodb.export_list()
     today = datetime.date.today()
-    # update.apply_async()
     return render_template("index.html", content=banks, date_time=today)
 
 
