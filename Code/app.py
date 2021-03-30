3e628d03943a194356f4e88d1ef00d8887c3545e
alexandremulina <49921028+alexandremulina@users.noreply.github.com>
2021-03-29 23:18:16 -0300

    BackgroundScheduler

--- Code/app.py
+++ Code/app.py
@@ -19,7 +19,7 @@ def update():
     mongodb.insert_list()
 
 sched = BackgroundScheduler(daemon=True)
-sched.add_job(update,'interval', minutes=15)
+sched.add_job(update,'interval', minutes=60)
 sched.start()
 
 
@@ -30,14 +30,11 @@ cache = Cache(app)
 
 
 @app.route("/")
-@cache.cached(timeout=3600) # 1 hour
+@cache.cached(timeout=3900)
 def home():
     banks = mongodb.export_list()
     today = datetime.date.today()
     return render_template("index.html", content=banks, date_time=today)
-
-
-
 
 
 
